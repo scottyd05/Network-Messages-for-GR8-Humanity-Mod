78bd04b45a7bf6906bded58c7506481441431a4a
scottyd05 <46728449+scottyd05@users.noreply.github.com>
2021-05-31 22:26:16 +0100

    Create CfgNetworkMessagesAdded.cpp

---
+++ CfgNetworkMessagesAdded.cpp
@@ -0,0 +1,52 @@
+////CfgNetworkMessagesAdded.cpp
+
+//drop this in the root mission file and add this to description.ext inside CfgNetworkMessages (#include "CfgNetworkMessagesAdded.cpp")
+
+class updateStatsRequest 
+{
+        module = "object_player";
+        parameters[] = 
+    {
+        "STRING",
+        "STRING",       
+        "SCALAR",
+        "SCALAR"
+    };
+};
+class createPlayerResponse
+{
+        module = "object_player";
+        parameters[] = 
+    {
+        "OBJECT",
+        "STRING",
+        "STRING",
+        "STRING",            
+        "SCALAR",
+        "SCALAR",
+        "SCALAR",
+        "SCALAR",
+        "SCALAR",
+        "SCALAR", 
+        "ARRAY",
+        "SCALAR"
+    }; 
+};
+class loadPlayerResponse
+{
+    module = "object_player";
+    parameters[] = 
+    {
+        "STRING",
+        "STRING",
+        "STRING",        
+        "SCALAR",
+        "SCALAR",
+        "SCALAR",
+        "SCALAR",
+        "SCALAR",
+        "ARRAY",
+        "SCALAR",
+        "SCALAR"
+    };
+};
